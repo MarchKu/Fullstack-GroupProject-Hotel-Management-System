81435d4a340e3c512b090477713ed72b001aed05
Apirak Fakin <apirakfakin@gmail.com>
2024-07-08 18:31:00 +0700

    feat: add responsive footer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Heading1 } from "lucide-react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
