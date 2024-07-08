0c0104e3b4e8ffeddb50e3cbd16e047befa63b9d
Apirak Fakin <apirakfakin@gmail.com>
2024-07-08 18:38:50 +0700

    refactor: remove unused files

--- components/FooterComponent.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-
-const FooterComponent = () => {
-  return <div className="w-screen bg-[#2F3E35] h-[485px]"></div>;
-};
-
-export default FooterComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
-"use client";
 import { Inter } from "next/font/google";
-import NavbarComponent from "@/components/NavbarComponent";
-import FooterComponent from "@/components/FooterComponent";
+import NavbarComponent from "@/components/navigation-component/NavbarComponent";
 
 export default function Home() {
   return (
     <>
       <NavbarComponent />
       <h1 className="text-8xl font-body">Hello</h1>
-      <h1 className="text-8xl font-body">Hello</h1>
-      <h1 className="text-8xl font-body">Hello</h1>
-      <h1 className="text-8xl font-body">Hello</h1>
-      <h1 className="text-8xl font-body">Hello</h1>
-      <FooterComponent />
+      
     </>
   );
 }
