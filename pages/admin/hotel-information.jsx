9dc04cbd01770f9b6f821b8386e144f3bbb644f8
Apirak Fakin <apirakfakin@gmail.com>
2024-07-12 13:19:07 +0700

    feat: add sidebar

---
+++ pages/admin/hotel-information.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../../components/hotel-information/Sidebar";
+
+const HotelInformation = () => {
+  return (
+    <div className="w-full flex bg-blue-500">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default HotelInformation;
