5b973de22f389728cddb3e6cefd57f553eaf00eb
Apirak Fakin <apirakfakin@gmail.com>
2024-07-15 01:12:57 +0700

    feat: start implementing the API to hotel information page

--- components/hotel-information/Main.jsx
+++ components/hotel-information/Main.jsx
@@ -1,45 +1,64 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import neatlyIcon from "../../assets/Navigation/neatlyLogo.png";
 import Image from "next/image";
+import axios from "axios";
 
 const Main = () => {
-  const [hotelData, setHotelData] = useState({
-    name: "Neatly Hotel",
-    description:
-      "Set in Bangkok, Thailand. Neatly Hotel offers 5-star accommodation with an outdoor pool, kids' club, sports facilities and a fitness centre. There is also a spa, an indoor pool and saunas. All units at the hotel are equipped with a seating area, a flat-screen TV with satellite channels, a dining area and a private bathroom with free toiletries, a bathtub and a hairdryer. Every room in Neatly Hotel features a furnished balcony. Some rooms are equipped with a coffee machine. Free WiFi and entertainment facilities are available at property and also rentals are provided to explore the area.",
-    logo: neatlyIcon,
-  });
+  const [hotelData, setHotelData] = useState({});
+  useEffect(() => {
+    const getHotelData = async () => {
+      const result = await axios.get("http://localhost:3000/api/getHotelData");
+      // console.log("result: ", result.data.data);
+      await setHotelData({
+        ...result.data.data,
+      });
+    };
+    getHotelData();
+    console.log(hotelData);
+  }, []);
+
+  const updateHotelData = async (data) => {
+    try {
+      await axios.post(
+        "http://localhost:3000/api/updateHotelProperties",
+        data,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleHotelName = (e) => {
     let newHotelData = { ...hotelData };
-    newHotelData.name = e.target.value;
+    newHotelData.hotel_name = e.target.value;
     setHotelData(newHotelData);
   };
   const handleHotelDescription = (e) => {
     let newHotelData = { ...hotelData };
-    newHotelData.description = e.target.value;
+    newHotelData.hotel_description = e.target.value;
     setHotelData(newHotelData);
   };
-  useEffect(() => {
-    console.log(hotelData);
-  }, [hotelData]);
+
+
   return (
-    <main className="w-full bg-[#F6F7FC]">
+    <form className="w-full bg-[#F6F7FC]">
       <section className="flex justify-between items-center px-[60px] py-[25px] bg-white h-[80px]">
         <h1 className="text-xl font-semibold">Hotel Information</h1>
         <button className="bg-[#C14817] text-white h-12 px-8 rounded">
           Update
         </button>
       </section>
-      <section>
-        <form
-          action=""
-          className="bg-white mx-[60px] my-10 py-10 px-[80px] rounded flex flex-col gap-10"
-        >
+      <section className="bg-white mx-[60px] my-10 py-10 px-[80px] rounded flex flex-col gap-10">
+        
           <div className="flex flex-col gap-1">
             <label htmlFor="">Hotel name *</label>
             <input
               type="text"
-              value={hotelData.name}
+              value={hotelData.hotel_name}
               className="border-[1px] border-[#D6D9E4] py-[6px] px-3 rounded"
               onChange={handleHotelName}
             />
@@ -48,7 +67,7 @@ const Main = () => {
             <label htmlFor="">Hotel description *</label>
             <textarea
               type="text"
-              value={hotelData.description}
+              value={hotelData.hotel_description}
               rows={6}
               className="border-[1px] border-[#D6D9E4] py-[6px] px-3 rounded resize-y"
               onChange={handleHotelDescription}
@@ -70,9 +89,9 @@ const Main = () => {
               />
             )}
           </div>
-        </form>
+        
       </section>
-    </main>
+    </form>
   );
 };
 
--- pages/admin/hotel-information.jsx
+++ pages/admin/hotel-information.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import Sidebar from "@/components/hotel-information/Sidebar";
 import Main from "@/components/hotel-information/Main";
 
+
 const HotelInformation = () => {
+
   return (
     <div className="w-full flex bg-blue-500">
         <Sidebar />
