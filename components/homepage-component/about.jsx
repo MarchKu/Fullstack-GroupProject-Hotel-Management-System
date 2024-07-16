b220ab8f6255da01941ea5b8cd1a93a8682fdd5a
Apirak Fakin <apirakfakin@gmail.com>
2024-07-16 16:34:02 +0700

    feat: apply hotel data to about section

--- components/homepage-component/about.jsx
+++ components/homepage-component/about.jsx
@@ -9,31 +9,19 @@ import {
 import { Card, CardContent } from "@/components/ui/card";
 import { carouselAbout } from "@/utils/carousel-info-array/carousel-about";
 
-const About = () => {
+const About = ({ hotelName, hotelDescription }) => {
   const carouselImg = carouselAbout;
   return (
-    <section className="flex flex-col w-full min-h-[1200px] h-screen md:h-[120vh] gap-[5%] xl:gap-[10%] py-[5%] overflow-hidden" id="about">
+    <section
+      className="flex flex-col w-full min-h-[1200px] h-screen md:h-[120vh] gap-[5%] xl:gap-[10%] py-[5%] overflow-hidden"
+      id="about"
+    >
       <div className="w-full h-[40%] flex flex-col  px-[5%] md:px-[10%]">
         <h1 className="h-[20%] font-heading text-center content-center text-primary-heading md:text-[5rem] text-[4rem] xl:text-[7rem] md:text-left">
-          Neatly Hotel
+          {hotelName}
         </h1>
         <div className=" h-[80%] flex flex-col font-body text-gray-700 gap-[1.5rem] text-[1rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.5rem] md:pl-[20%] md:pt-[5%]">
-          <p>
-            Set in Bangkok, Thailand. Neatly Hotel offers 5-star accommodation
-            with an outdoor pool, kids' club, sports facilities and a fitness
-            centre. There is also a spa, an indoor pool and saunas.
-          </p>
-          <p>
-            All units at the hotel are equipped with a seating area, a
-            flat-screen TV with satellite channels, a dining area and a private
-            bathroom with free toiletries, a bathtub and a hairdryer. Every room
-            in Neatly Hotel features a furnished balcony. Some rooms are
-            equipped with a coffe e machine.
-          </p>
-          <p>
-            Free WiFi and entertainment facilities are available at property and
-            also rentals are provided to explore the area.
-          </p>
+          <p>{hotelDescription}</p>
         </div>
       </div>
       <div className="w-screen h-[50%] flex justify-center">
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,20 +8,32 @@ import Testimonial from "@/components/homepage-component/testimonial";
 import FooterComponent from "@/components/footer-component/FooterComponent";
 import NavbarComponent from "@/components/navigation-component/NavbarComponent";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [hotelData, setHotelData] = useState({});
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     setIsAuthenticated(Boolean(token));
+
+    const getHotelData = async () => {
+      const result = await axios.get("http://localhost:3000/api/getHotelData");
+      setHotelData(result.data.data);
+    };
+
+    getHotelData();
   }, []);
 
   return (
     <>
       <NavbarComponent isAuthenticated={isAuthenticated} />
       <Hero />
-      <About />
+      <About
+        hotelName={hotelData.hotel_name}
+        hotelDescription={hotelData.hotel_description}
+      />
       <Service />
       <RoomSuite />
       <Testimonial />
