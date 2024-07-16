7c10e616f30450e4a7c4a756438b653b6e5f76b3
Apirak Fakin <apirakfakin@gmail.com>
2024-07-16 16:23:45 +0700

    feat: apply hotel data to navbar

--- components/navigation-component/NavbarComponent.jsx
+++ components/navigation-component/NavbarComponent.jsx
@@ -10,11 +10,13 @@ import UserMenuDesktop from "./UserMenuDesktop";
 import Logo from "./Logo";
 import NavLinkDesktop from "./NavLinkDesktop";
 import NonUserMenuMobile from "./NonUserMenuMobile";
+import axios from "axios";
 import { useAuth } from "@/contexts/authentication";
 import UserImage from "../../assets/Navigation/UserImage.png";
 
 const NavbarComponent = ({ isAuthenticated }) => {
   const [user, setUser] = useState({});
+  const [hotelData, setHotelData] = useState({})
   useEffect(() => {
     const fetchUserData = async () => {
       const userData = localStorage.getItem("user");
@@ -23,29 +25,31 @@ const NavbarComponent = ({ isAuthenticated }) => {
         setUser(parsedData);
       }
     };
-    fetchUserData();  
+    const getHotelData = async () => {
+      const result = await axios.get("http://localhost:3000/api/getHotelData");
+      setHotelData(result.data.data);
+    };
+    getHotelData()
+    fetchUserData();
   }, []);
 
   useEffect(() => {
     console.log("user:", user);
-    console.log("user:", user.fullName);
-  }, [user]);
+    console.log("hotel:", hotelData);
+  }, [user, hotelData]);
   const AuthenticatedUser = (
     <NavigationMenu className="flex items-center min-h-[48px] md:min-h-[100px] h-[5vh] border-[1px] border-[#E4E6ED] justify-center w-full">
       <div className="flex justify-between w-full px-[5%] xl:px-[10%]">
         <div className="flex justify-between text-[14px]  md:w-full">
           <div className="w-full flex items-center justify-between">
-            <Logo />
-            <NavLinkDesktop />
+            <Logo hotelLogo={ hotelData.hotel_logo} />
+            <NavLinkDesktop hotelName={ hotelData.hotel_name} />
           </div>
         </div>
         <div className="flex items-center justify-end">
           <Notification />
           <UserMenuMobile image={user.profilePicture} name={user.fullName} />
-          <UserMenuDesktop
-            image={user.profilePicture}
-            name={ user.fullName}
-          />
+          <UserMenuDesktop image={user.profilePicture} name={user.fullName} />
         </div>
       </div>
     </NavigationMenu>
@@ -56,15 +60,15 @@ const NavbarComponent = ({ isAuthenticated }) => {
       <div className="flex justify-between w-full px-[5%] xl:px-[10%]">
         <div className="flex justify-between text-[14px]  w-full">
           <div className="w-full flex items-center justify-between">
-            <Logo />
-            <NavLinkDesktop />
-            <NonUserMenuMobile />
+            <Logo hotelLogo={ hotelData.hotel_logo} />
+            <NavLinkDesktop hotelName={ hotelData.hotel_name}/>
+            <NonUserMenuMobile hotelName={ hotelData.hotel_name}/>
           </div>
-          <div className="flex items-center justify-end">
+          <div className="hidden md:flex items-center justify-end">
             <Link href="/login" legacyBehavior passHref>
               <NavigationMenuLink className="text-[1rem] ml-2 leading-4 font-semibold text-[#E76B39]">
                 <p className="whitespace-nowrap">Log in</p>
-              </NavigationMenuLink> 
+              </NavigationMenuLink>
             </Link>
           </div>
         </div>
