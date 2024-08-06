74b341dd66ffbee51f9cf4057f7a377bffd83adb
Apirak Fakin <apirakfakin@gmail.com>
2024-08-07 04:03:13 +0700

    style: about section

--- components/homepage-component/about.jsx
+++ components/homepage-component/about.jsx
@@ -13,18 +13,18 @@ const About = ({ hotelName, hotelDescription }) => {
   const carouselImg = carouselAbout;
   return (
     <section
-      className="flex flex-col w-full min-h-[1200px] h-screen md:h-[120vh] gap-[5%] xl:gap-[10%] py-[5%] overflow-hidden"
+      className="flex flex-col w-full max-w-[1440px] md:h-[120vh] gap-10 xl:gap-[10%] py-10 md:pt-[115px] px-4 overflow-hidden"
       id="about"
     >
-      <div className="w-full h-[40%] flex flex-col  px-[5%] md:px-[10%]">
-        <h1 className="h-[20%] font-heading text-center content-center text-primary-heading md:text-[5rem] text-[4rem] xl:text-[7rem] md:text-left">
+      <div className="w-full flex flex-col gap-10 md:gap-[52px] md:px-[10%]">
+        <h1 className="font-heading text-primary-heading md:text-[68px] text-[44px] leading-[55px] xl:text-[7rem] md:text-left">
           {hotelName}
         </h1>
-        <div className=" h-[80%] flex flex-col font-body text-gray-700 gap-[1.5rem] text-[1rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.5rem] md:pl-[20%] md:pt-[5%]">
+        <div className=" flex flex-col font-body text-gray-700 gap-[1.5rem] text-base md:text-base lg:text-[1rem] xl:text-[1.5rem] md:pl-[192px]">
           <p>{hotelDescription}</p>
         </div>
       </div>
-      <div className="w-screen h-[50%] flex justify-center">
+      <div className="w-full h-[225px] md:h-[500px] flex justify-center">
         <Carousel
           opts={{
             align: "center",
@@ -34,11 +34,11 @@ const About = ({ hotelName, hotelDescription }) => {
         >
           <CarouselContent className="w-full h-full ">
             {carouselImg.map((img, index) => (
-              <CarouselItem key={index}>
-                <Card className="w-screen md:w-full h-full">
+              <CarouselItem key={index} className="basis-1/3">
+                <Card className="">
                   <CardContent className="h-full flex items-center justify-center p-0">
                     <div
-                      className="size-full bg-center bg-cover bg-slate-300 bg-blend-multiply"
+                      className="w-full h-[225px] md:h-[500px] bg-center bg-cover bg-slate-300 bg-blend-multiply"
                       style={{ backgroundImage: `url(${img})` }}
                     ></div>
                   </CardContent>
