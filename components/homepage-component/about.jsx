29325b278d063d8171985e76ce053536ccbe0496
Kunakorn <kunakorn.ph98@gmail.com>
2024-08-08 16:13:29 +0700

    style: adjust style of landing page and login page

--- components/homepage-component/about.jsx
+++ components/homepage-component/about.jsx
@@ -13,45 +13,47 @@ const About = ({ hotelName, hotelDescription }) => {
   const carouselImg = carouselAbout;
   return (
     <section
-      className="flex flex-col w-full max-w-[1440px] gap-10 md:gap-[134px] py-10 md:pt-[115px] px-4 overflow-hidden"
+      className="flex flex-col w-full justify-start items-center  py-10 md:pt-[115px] px-4 overflow-hidden"
       id="about"
     >
-      <div className="w-full flex flex-col gap-10 md:gap-[52px] md:px-[10%]">
-        <h1 className="font-heading text-primary-heading md:text-[68px] text-[44px] leading-[55px] xl:text-[7rem] md:text-left">
-          {hotelName}
-        </h1>
-        <div className=" flex flex-col font-body text-gray-700 gap-[1.5rem] text-base md:text-base lg:text-[1rem] xl:text-[1.5rem] md:pl-[192px]">
-          <p>{hotelDescription}</p>
+      <div className="size-full flex flex-col max-w-[1440px] gap-[52px]">
+        <div className="w-full h-auto flex flex-col gap-10 md:gap-[52px] md:px-[10%]">
+          <h1 className="w-full h-auto font-heading text-primary-heading text-[4rem] md:text-[6rem]">
+            {hotelName}
+          </h1>
+          <div className="w-full h-auto font-body text-primary-heading xl:text-[1.25rem] pl-[20%]">
+            <p>{hotelDescription}</p>
+          </div>
         </div>
-      </div>
-      <div className="w-full h-[225px] md:h-[500px] flex justify-center">
-        <Carousel
-          opts={{
-            align: "center",
-            loop: true,
-          }}
-          className="w-full h-full"
-        >
-          <CarouselContent className="w-full h-full ">
-            {carouselImg.map((img, index) => (
-              <CarouselItem
-                key={index}
-                className="basis-[180px] md:basis-[400px]"
-              >
-                <Card className="">
-                  <CardContent className="h-full flex items-center justify-center p-0">
-                    <div
-                      className="w-full h-[225px] md:h-[500px] bg-center bg-cover bg-slate-300 bg-blend-multiply"
-                      style={{ backgroundImage: `url(${img})` }}
-                    ></div>
-                  </CardContent>
-                </Card>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+        <div className="w-full h-[225px] md:h-[500px] flex justify-center">
+          <Carousel
+            opts={{
+              align: "center",
+              loop: true,
+            }}
+            className="w-full h-full"
+          >
+            <CarouselContent className="w-full h-full ">
+              {carouselImg.map((img, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-[180px] md:basis-[400px]"
+                >
+                  <Card className="">
+                    <CardContent className="h-full flex items-center justify-center p-0">
+                      <div
+                        className="w-full h-[225px] md:h-[500px] bg-center bg-cover bg-slate-300 bg-blend-multiply"
+                        style={{ backgroundImage: `url(${img})` }}
+                      ></div>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
       </div>
     </section>
   );
--- components/homepage-component/hero.jsx
+++ components/homepage-component/hero.jsx
@@ -9,16 +9,18 @@ import { SearchBox } from "../search-component/SearchBox";
 const Hero = () => {
   return (
     <section
-      className="w-full md:h-[900px] flex flex-col items-center gap-[57px] md:gap-[124px] px-4 pb-[46px] pt-[100px] md:pt-[215px] bg-center bg-cover bg-slate-300 bg-blend-multiply "
+      className="w-full h-screen md:h-[80vh] flex flex-col justify-center items-center gap-[57px] md:gap-[124px] px-4 pb-[46px]  bg-center bg-cover bg-slate-300 bg-blend-multiply "
       style={{ backgroundImage: `url(${background.src})` }}
     >
-      <div className="font-heading  text-center text-white flex flex-col justify-center items-center ">
-        <h1 className="w-full text-[44px] md:text-[88px]">
-          A Best Place for Your Neatly Experience
-        </h1>
-      </div>
-      <div className="w-full h-[50%] md:h-[15%] xl:h-[20%] md:px-[2.5%] px-[5%] py-[10%] md:py-[5%] xl:py-[3%] xl:px-10% bg-white rounded-lg">
-        <SearchBox onDateChage={() => true} />
+      <div className="size-full max-w-[1440px] flex flex-col justify-center items-center gap-[1.5rem] md:gap-[10%]">
+        <div className="font-heading text-center text-white flex flex-col justify-center items-center ">
+          <h1 className="w-full text-[44px] md:text-[88px] xl:text-[6rem]">
+            A Best Place for Your Neatly Experience
+          </h1>
+        </div>
+        <div className="w-full h-auto md:h-auto md:px-[2.5%] px-[5%] py-[10%] md:py-[5%] xl:py-[3%] xl:px-10% bg-white rounded-lg">
+          <SearchBox onDateChage={() => true} />
+        </div>
       </div>
     </section>
   );
