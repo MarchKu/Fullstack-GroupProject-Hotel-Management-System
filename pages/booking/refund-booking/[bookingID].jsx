36df3c67e6723082453efed95df2806bd6f019f0
Kunakorn <kunakorn.ph98@gmail.com>
2024-08-13 10:49:59 +0700

    fix:Fix refund

--- pages/booking/refund-booking/[bookingID].jsx
+++ pages/booking/refund-booking/[bookingID].jsx
@@ -55,7 +55,7 @@ const BookingHistory = () => {
 
   const cancleClick = () => {
     if (bookingID) {
-      /* cancelBooking(bookingID); */
+      cancelBooking(bookingID);
       setIsCancel(true);
     }
   };
